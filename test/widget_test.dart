3eba7d987646dc4bf8c6d40535e50aa65af41dfa
Eddels <bettebutt8r@gmail.com>
2026-02-14 21:22:17 +0100

    Rename test from KobPayApp to Myapp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kobpay_mobile/main.dart';
 
 void main() {
-  testWidgets('KobPayApp builds', (WidgetTester tester) async {
-    await tester.pumpWidget(const KobPayApp());
+  testWidgets('Myapp builds', (WidgetTester tester) async {
+    await tester.pumpWidget(const Myapp());
     await tester.pumpAndSettle();
-    expect(find.byType(KobPayApp), findsOneWidget);
+    expect(find.byType(Myapp), findsOneWidget);
   });
 }
